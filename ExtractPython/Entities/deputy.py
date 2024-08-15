17d057f8644ddb6421565b0b596f378a14962ca5
Vinicius Caracho <129306009+Viniciuscaracho@users.noreply.github.com>
2024-08-15 16:18:57 -0300

    details

--- ExtractPython/Entities/deputy.py
+++ ExtractPython/Entities/deputy.py
@@ -8,15 +8,18 @@ class Deputy:
 
     def get_deputies(self, authors_df):
         deputies_dict = {}
+        # Obtém URIs únicas dos autores para evitar chamadas duplicadas à API
         unique_uris = authors_df['uri'].unique()
 
         for author_uri in unique_uris:
             try:
                 response = requests.get(author_uri)
                 response_data = response.json()['dados']
+                # Obtém a lista de proposições associadas ao autor atual
                 author_propositions = authors_df[authors_df['uri'] == author_uri]['proposition_id'].tolist()
                 deputy_id = response_data['id']
 
+                # Se o deputado ainda não estiver no dicionário, adicione-o com suas informações
                 if deputy_id not in deputies_dict:
                     deputies_dict[deputy_id] = {
                         'id': deputy_id,
@@ -25,13 +28,17 @@ class Deputy:
                         'proposition_ids': author_propositions
                     }
                 else:
+                    # Se o deputado já estiver no dicionário, apenas adicione os IDs das novas proposições
                     deputies_dict[deputy_id]['proposition_ids'].extend(author_propositions)
 
             except requests.exceptions.RequestException as e:
+                # Tratamento de erro em caso de falha na solicitação
                 print(f"Error fetching data from {author_uri}: {str(e)}")
 
+        # Remove IDs de proposições duplicados para cada deputado
         for deputy in deputies_dict.values():
             deputy['proposition_ids'] = list(set(deputy['proposition_ids']))
 
+        # Converte o dicionário de deputados em um DataFrame e retorna
         deputies_df = pd.DataFrame(deputies_dict.values())
         return deputies_df
--- ExtractPython/Entities/propositions.py
+++ ExtractPython/Entities/propositions.py
@@ -14,20 +14,24 @@ class Proposition:
 
     def get_proposition_authors(self, batch_size=100):
         authors_list = []
+        # Itera sobre os IDs das proposições em lotes
         for batch in self.chunks(self.selected_propositions_df['id'], batch_size):
             for proposition_id in batch:
                 try:
                     response = requests.get(f"{self.base_url}/proposicoes/{proposition_id}/autores")
                     response.raise_for_status()
                 except requests.exceptions.RequestException as e:
+                    # Tratamento de erro em caso de falha na solicitação
                     print(f"Error fetching authors for proposition {proposition_id}: {str(e)}")
                     continue
 
                 response_data = response.json()
                 if 'dados' not in response_data:
+                    # Caso não haja dados de autores na resposta
                     print(f"No authors found for proposition {proposition_id}")
                     continue
 
+                # Adiciona cada autor à lista de autores, ignorando órgãos
                 for author in response_data['dados']:
                     if '/orgaos/' in author["uri"]:
                         continue
@@ -44,15 +48,19 @@ class Proposition:
         file_path = os.path.join(base_dir, '../proposicoes-2024.json')
 
         try:
+            # Tenta ler as proposições do arquivo JSON local
             propositions = pd.read_json(file_path)
         except FileNotFoundError as e:
+            # Tratamento de erro para arquivo não encontrado
             print(f"File not found error: {str(e)}")
             return pd.DataFrame()
         except ValueError as e:
+            # Tratamento de erro para problemas na leitura do JSON
             print(f"Error reading JSON file: {str(e)}")
             return pd.DataFrame()
 
         if 'dados' not in propositions.columns:
+            # Verifica se o JSON possui a estrutura esperada
             print("Unexpected JSON structure.")
             return pd.DataFrame()
 
@@ -68,6 +76,7 @@ class Proposition:
             if max_rows <= len(propositions_list):
                 break
 
+        # Solicita proposições adicionais da API
         api_url = f"{self.base_url}/proposicoes"
         response = requests.get(api_url, params={'itens': max_rows})
 
@@ -86,4 +95,3 @@ class Proposition:
             print(f"API request failed with status code: {response.status_code}")
 
         return pd.DataFrame(propositions_list)
-
